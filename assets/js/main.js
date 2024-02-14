f1b66fe39d67de1a95a1c6291a334c27dc5a0b45
Sajan Chhetri <159753433+Digitalsajan10@users.noreply.github.com>
2024-02-14 16:58:40 +0545

    d

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,47 +1,46 @@
 /*===== MENU SHOW =====*/ 
-const showMenu = (toggleId, navId) => {
-    const toggle = document.getElementById(toggleId);
-    const nav = document.getElementById(navId);
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
 
-    if (toggle && nav) {
-        toggle.addEventListener('click', () => {
-            nav.classList.toggle('show');
-        });
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
     }
-};
-showMenu('nav-toggle', 'nav-menu');
+}
+showMenu('nav-toggle','nav-menu')
 
 /*==================== REMOVE MENU MOBILE ====================*/
-const navLinks = document.querySelectorAll('.nav__link');
+const navLink = document.querySelectorAll('.nav__link')
 
-function linkAction() {
-    const navMenu = document.getElementById('nav-menu');
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
     // When we click on each nav__link, we remove the show-menu class
-    navMenu.classList.remove('show');
+    navMenu.classList.remove('show')
 }
-navLinks.forEach(navLink => navLink.addEventListener('click', linkAction));
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
-const sections = document.querySelectorAll('section[id]');
+const sections = document.querySelectorAll('section[id]')
 
-const scrollActive = () => {
-    const scrollDown = window.scrollY;
-    const navMenuLinks = document.querySelectorAll('.nav__menu a');
+const scrollActive = () =>{
+    const scrollDown = window.scrollY
 
-    sections.forEach((current, index) => {
-        const sectionHeight = current.offsetHeight;
-        const sectionTop = current.offsetTop - 58;
-        const sectionId = current.getAttribute('id');
-        const navMenuLink = navMenuLinks[index];
-
-        if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
-            navMenuLink.classList.add('active-link');
-        } else {
-            navMenuLink.classList.remove('active-link');
-        }
-    });
-};
-window.addEventListener('scroll', scrollActive);
+  sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id'),
+              sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+        
+        if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+            sectionsClass.classList.add('active-link')
+        }else{
+            sectionsClass.classList.remove('active-link')
+        }                                                    
+    })
+}
+window.addEventListener('scroll', scrollActive)
 
 /*===== SCROLL REVEAL ANIMATION =====*/
 const sr = ScrollReveal({
@@ -49,7 +48,7 @@ const sr = ScrollReveal({
     distance: '60px',
     duration: 2000,
     delay: 200,
-    // reset: true
+//     reset: true
 });
 
 sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{}); 
