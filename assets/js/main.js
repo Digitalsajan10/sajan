ddc925f3b429e30351154f2d841278dd6800ae6a
Sajan Chhetri <159753433+Digitalsajan10@users.noreply.github.com>
2024-03-09 16:25:29 +0545

    x

--- assets/js/main.js
+++ assets/js/main.js
@@ -64,27 +64,3 @@ document.querySelectorAll('.Blogs .read-more-btn').forEach(btn => {
         this.textContent = fullContent.style.display === 'none' ? 'Read more' : 'Read less';
     });
 });
-
-
-const ebookButtons = document.querySelectorAll('.ebook-button');
-
-ebookButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const url = button.getAttribute('data-ebook-url');
-    downloadFile(url, 'ebook.pdf');
-  });
-});
-
-function downloadFile(url, fileName) {
-  fetch(url, { method: 'get', mode: 'no-cors', referrerPolicy: 'no-referrer' })
-    .then(res => res.blob())
-    .then(res => {
-      const aElement = document.createElement('a');
-      aElement.setAttribute('download', fileName);
-      const href = URL.createObjectURL(res);
-      aElement.href = href;
-      aElement.setAttribute('target', '_blank');
-      aElement.click();
-      URL.revokeObjectURL(href);
-    });
-}
