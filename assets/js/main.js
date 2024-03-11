0b2c6ff1a1be653d8780bbcae5a67e836edfc8b7
Sajan Chhetri <159753433+Digitalsajan10@users.noreply.github.com>
2024-03-11 08:19:18 +0545

    s

--- assets/js/main.js
+++ assets/js/main.js
@@ -65,11 +65,25 @@ document.querySelectorAll('.Blogs .read-more-btn').forEach(btn => {
     });
 });
 
-<script>
-    function shareToFacebook(title, url, image) {
-        // Construct the Facebook share URL
-        let facebookShareUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(url)}&quote=${encodeURIComponent(title)}`;
-        // Open the Facebook share dialog
-        window.open(facebookShareUrl, '_blank');
-    }
-</script>
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'uil-sun';
+
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun';
+
+if (selectedTheme) {
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+  themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme);
+}
+
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle(darkTheme);
+    themeButton.classList.toggle(iconTheme);
+    localStorage.setItem('selected-theme', getCurrentTheme());
+    localStorage.setItem('selected-icon', getCurrentIcon());
+});
+
